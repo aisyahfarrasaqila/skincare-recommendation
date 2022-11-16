1b5defd76efe7fe84aea8467bd84572165ebbef0
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2022-11-16 10:07:18 +0700

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tugas IF4043 Sistem Informasi Lanjut
+
+- 13519040 - Shafira Naya Aprisadianti
+- 13519054 - Aisyah Farras Aqila
+- 13519126 - Alvin Rizqi Alfisyahrin
+
+## Cara menjalankan web di lokal
+
+```bash
+git clone https://github.com/shafiranaya/skincare-recommendation.git
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from main import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/clustering', methods=['GET','POST'])
+def clustering():
+    # product_name_input = request.form.get("inputProductName")
+    # global product_name
+    # product_name = get_recommendation(product_name_input)
+    # input_list = [0 for i in range(20)]
+    # input_list = list(map(int,request.form.values()))
+    global cluster_idx
+    if request.method == 'POST':
+        submitted = True
+        input_list = list(map(int,request.form.values()))
+    else:
+        submitted = False
+        input_list = [0 for i in range(20)]
+    cluster_idx = get_cluster(input_list)
+    return render_template('clustering.html',result=cluster_idx, submitted=submitted,inputan=input_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
